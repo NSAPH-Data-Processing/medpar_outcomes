6e73d4bd10435c8dbbd36e04ea1133af61f7cbd1
shreyanalluri <nallshrey@gmail.com>
2025-03-12 10:40:59 -0400

    updated icd parsing function and args

--- src/get_outcomes.py
+++ src/get_outcomes.py
@@ -11,42 +11,37 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
-def read_conditional_icd_codes(icd_yml, outcome):
+def read_icd_codes(icd_yml, outcome, conditional=False):
     with open(icd_yml, 'r') as f:
         icd_dict = yaml.load(f, Loader=yaml.FullLoader)
 
-    primary_icd9 = icd_dict[outcome].get("primary_icd9", [])
-    primary_icd10 = icd_dict[outcome].get("primary_icd10", [])
-    secondary_icd9 = icd_dict[outcome].get("secondary_icd9", [])
-    secondary_icd10 = icd_dict[outcome].get("secondary_icd10", [])
+    if conditional:
+        primary_icd9 = icd_dict[outcome].get("primary_icd9", [])
+        primary_icd10 = icd_dict[outcome].get("primary_icd10", [])
+        secondary_icd9 = icd_dict[outcome].get("secondary_icd9", [])
+        secondary_icd10 = icd_dict[outcome].get("secondary_icd10", [])
 
-    primary_icd = primary_icd9 + primary_icd10
-    secondary_icd = secondary_icd9 + secondary_icd10
+        primary_icd = primary_icd9 + primary_icd10
+        secondary_icd = secondary_icd9 + secondary_icd10
 
-    # Convert to properly formatted SQL strings
-    primary_icd_string = ",".join([f"'{code}'" for code in primary_icd])
-    secondary_icd_string = ",".join([f"'{code}'" for code in secondary_icd])
+        primary_icd_string = ",".join([f"'{code}'" for code in primary_icd])
+        secondary_icd_string = ",".join([f"'{code}'" for code in secondary_icd])
 
-    return primary_icd_string, secondary_icd_string
+        return primary_icd_string, secondary_icd_string
+    else:
+        icd_string = (
+            ",".join([f"'{x}'" for x in icd_dict[outcome]["icd9"]]) + 
+            "," +
+            ",".join([f"'{x}'" for x in icd_dict[outcome]["icd10"]])
+        )
 
-def read_icd_string(icd_yml, outcome):
-    with open(icd_yml, 'r') as f:
-        icd_dict = yaml.load(f, Loader=yaml.FullLoader)
+        return icd_string
 
-    icd_string = (
-        ",".join([f"'{x}'" for x in icd_dict[outcome]["icd9"]]) + 
-        "," +
-        ",".join([f"'{x}'" for x in icd_dict[outcome]["icd10"]])
-    )
-
-    return icd_string
-
-def get_outcome_query(outcome_criteria, icd_yml, outcome, hosp_prefix, year, first_n = None):
-    LOGGER.info(f"Generating query for outcome: {outcome} for year: {year}")
+def get_outcome_query(outcome_criteria, icd_string, hosp_prefix, year, first_n=None, icd_yml=None, outcome=None):
+    print("## Preparing query----")
     file = f"{hosp_prefix}_{year}.parquet"
 
     if outcome_criteria == 'all':
-        icd_string = read_icd_string(icd_yml, outcome)
         query = f"""
         SELECT bene_id, adm_id
         FROM (SELECT bene_id, adm_id, UNNEST(diagnoses) AS diag FROM '{file}')
@@ -55,29 +50,29 @@ def get_outcome_query(outcome_criteria, icd_yml, outcome, hosp_prefix, year, fir
         """
 
     elif outcome_criteria == 'first_n' and first_n is not None:
-        icd_string = read_icd_string(icd_yml, outcome)
-        conditions = " OR ".join([f"diagnoses[{i}] IN ({icd_string})" for i in range(1, first_n +1)])
+        conditions = " OR ".join([f"diagnoses[{i}] IN ({icd_string})" for i in range(1, first_n + 1)])
         query = f"""
         SELECT bene_id, adm_id
         FROM '{file}'
         WHERE {conditions}
         GROUP BY bene_id, adm_id
         """
+        
+    elif outcome_criteria == "conditional":
+        primary_icd, secondary_icd = read_icd_codes(icd_yml, outcome, conditional=True)
 
-    elif outcome_criteria == 'conditional':
-        primary_icd, secondary_icd = read_conditional_icd_codes(icd_yml, outcome)
         max_secondary_pos = 25
         secondary_conditions = " OR ".join(
             f"diagnoses[{i}] IN ({secondary_icd})"
             for i in range(2, max_secondary_pos + 1)
         )
         query = f"""
-        SELECT bene_id, adm_id,diagnoses, 
+        SELECT bene_id, adm_id, diagnoses, 
         diagnoses[1] AS primary_dx,
         LIST_SLICE(diagnoses, 2, {max_secondary_pos}) AS secondary_dx
         FROM '{file}'
         WHERE diagnoses[1] IN ({primary_icd})
-          AND ({secondary_conditions})
+        AND ({secondary_conditions})
         GROUP BY bene_id, adm_id, diagnoses
         """
 
@@ -102,7 +97,12 @@ def main(args):
         print(f"Preparing {outcome}----")
         outcome_criteria = args.outcome_criteria
 
-        query = get_outcome_query(outcome_criteria, args.icd_yml, outcome, args.hosp_prefix, args.year, args.first_n)
+        if outcome_criteria == "conditional":
+            icd_string = None
+        else:
+            icd_string = read_icd_codes(args.icd_yml, outcome)
+
+        query = get_outcome_query(outcome_criteria, icd_string, args.hosp_prefix, args.year, args.first_n, args.icd_yml, outcome)
         outcome_df = conn.execute(query).fetchdf()
         
         outcome_df['outcome'] = outcome
@@ -143,14 +143,14 @@ if __name__ == "__main__":
                         type=int
                        )
     parser.add_argument("--icd_yml",
-                        default = "./conf/icd_codes/icd_codes_11.yml"
+                        default = "./conf/icd_codes/xiao_00.yml"
                         )
     parser.add_argument("--hosp_prefix", 
-                        default = "./data/input/dw_legacy_medicare_00_16/adm"
+                        default = "./data/input/mbsf_medpar_denom/inpatient"
                        )
     parser.add_argument("--outcome_criteria",
-                        default = "all",
-                        choices = ["all", "first_n", "conditional"]
+                        default = "conditional",
+                        choices = ["all", "first_n","conditional"]
                         )
     parser.add_argument("--first_n",
                         default = 1,
@@ -162,9 +162,10 @@ if __name__ == "__main__":
                         choices=["parquet", "feather", "csv"]
                        )           
     parser.add_argument("--output_prefix", 
-                        default = "./data/intermediate/outcomes"
+                        default = "./data/output/medpar_outcomes/xiao_00/pd_lewy_outcomes"
                         )
     args = parser.parse_args()
     
     main(args)
 
+    
